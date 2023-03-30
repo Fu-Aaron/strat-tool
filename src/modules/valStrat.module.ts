2ad282b1b2aa54e181c1d6c6aede9534d8288f62
Aaron Fu <aaronfurules@gmail.com>
2023-03-29 19:15:31 -0700

    Refactor: removing repository folder to be in service following nestjs examples

--- src/modules/valStrat.module.ts
+++ src/modules/valStrat.module.ts
@@ -2,12 +2,11 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ValStratController } from "src/controller/valStrat.controller";
 import { ValStrat, ValStratSchema } from "src/model/valStrat.schema";
-import { ValStratRepository } from "src/repositories/valStrat.repository";
 import { ValStratService } from "src/service/valStrat.service";
 
 @Module({
     imports: [MongooseModule.forFeature([{name: ValStrat.name, schema: ValStratSchema}])],
     controllers: [ValStratController],
-    providers: [ValStratService, ValStratRepository],
+    providers: [ValStratService],
 })
 export class ValStratModule {}
--- src/service/valStrat.service.ts
+++ src/service/valStrat.service.ts
@@ -1,18 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateValStratDto } from 'src/model/createValStrat.dto';
-import { ValStratRepository } from 'src/repositories/valStrat.repository';
+import { ImageWithDescription } from 'src/model/mapImage.schema';
+import { ValStrat } from 'src/model/valStrat.schema';
 
 @Injectable()
 export class ValStratService {
-  constructor(private valStratRepository: ValStratRepository) {}
+  constructor(@InjectModel(ValStrat.name) private stratModel: Model<ValStrat>) {}
   getHello(): string {
     return 'Hello World!';
   }
 
   async uploadStrat(createValStratDto: CreateValStratDto): Promise<string> {
-    console.log("service:" + createValStratDto)
-    const valStrat = await this.valStratRepository.createStrat(createValStratDto);
-    console.log("hi")
-    return valStrat.toJSON().toString();
+    let imagesWithDescriptions: ImageWithDescription[] = this.createImageWithDescriptionsFromCreateStratDto(createValStratDto);
+        let strat = new this.stratModel({
+            name: createValStratDto.name,
+            notes: createValStratDto.notes,
+            map: createValStratDto.map,
+            agents: createValStratDto.agents,
+            imageWithDescription: imagesWithDescriptions,
+        });
+        const valStrat = await strat.save()
+        return JSON.stringify(valStrat)
+  }
+
+  private createImageWithDescriptionsFromCreateStratDto(createStratDto: CreateValStratDto) {
+    let imagesWithDescriptions: ImageWithDescription[] = []; 
+    for (let i = 0; i < createStratDto.mapFileNames.length; i++) {
+        const filename = createStratDto.mapFileNames[i]
+        let description;
+        if (i < createStratDto.mapDescriptions.length) {
+            description = createStratDto.mapDescriptions[i]
+        }
+        imagesWithDescriptions.push({filename, description})
+    }
+    return imagesWithDescriptions;
   }
 }
